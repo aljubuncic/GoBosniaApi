da253847548522ee288e4389ee1f97db4190385c
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-02 23:10:12 +0100

    Tetsing controller

---
+++ Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using GoTravnikApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoTravnikApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Event>>> Get() 
+        {
+            var events = new List<Event>
+            {
+                new Event
+                {
+                    Id = 1,
+                    Title = "Test",
+                    Description = "Test",
+                }
+            };
+            return Ok(events);
+        }
+        
+    }
+}
