f96e430b01fd7f049b0d4510c42aad77d3f289df
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-08 22:19:02 +0100

    testing

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using GoTravnikApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTravnikApi.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Attraction> Attractions { get; set; }
+    }
+}
---
+++ Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace GoTravnikApi.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
