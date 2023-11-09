397319de4010dd82ffae9e5f0ac1cab80cd4d85e
aljubuncic <ahmedlj07@gmail.com>
2023-11-09 15:12:43 +0100

    modified Rating model

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -1,8 +1,14 @@
-﻿namespace GoTravnikApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GoTravnikApi.Models
 {
     public class Rating
     {
         public int Id { get; set; }
         public int Value { get; set; }
+        public string TextComment { get; set; }
+        [ForeignKey("TouristContent")]
+        public int IdTouristContent { get; set;}
+        public TouristContent TouristContent { get; set;}
     }
 }
