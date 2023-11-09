492aa8bf3bc852acfd8980655ddeb786cf428cd0
aljubuncic <ahmedlj07@gmail.com>
2023-11-09 15:11:52 +0100

    modified Location class

--- Models/Location.cs
+++ Models/Location.cs
@@ -1,6 +1,29 @@
-﻿namespace GoTravnikApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoTravnikApi.Models
 {
     public class Location
     {
+
+        [Key]
+        public int Id { get; set; }
+        public double XCoordinate {  get; set; }
+        public double YCoordinate { get; set; }
+
+        public string Address { get; set; }
+
+        public Location()
+        {
+        }
+
+        public Location(int id, double xCoordinate, double yCoordinate, string address)
+        {
+            Id = id;
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+            Address = address;
+        }
+
+
     }
 }
