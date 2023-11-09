36fcb573b80fbfc5769a23202f926d04f6265816
aljubuncic <ahmedlj07@gmail.com>
2023-11-09 15:10:19 +0100

    modified Activity class

---
+++ Models/Activity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoTravnikApi.Models
+{
+    public class Activity : TouristContent
+    {
+        
+        public Activity()
+        {
+        }
+
+        public Activity(int id, string name, string description, string type, int idLocation, Location location, string image)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            IdLocation = idLocation;
+            Location = location;
+            Image = image;
+        }
+    }
+}
+
