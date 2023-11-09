34cb52284b59fcfd8efc0a1f6e55db0c34de47cf
aljubuncic <ahmedlj07@gmail.com>
2023-11-09 15:13:02 +0100

    added Subcategory class

---
+++ Models/Subcategory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GoTravnikApi.Models
+{
+    public class Subcategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("TouristContent")]
+        public int IdTouristContent {  get; set; }
+
+        public TouristContent TouristContent { get; set; }
+    }
+}
