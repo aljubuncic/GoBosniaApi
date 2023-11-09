2be8a9ed33e734cc95f8d05de24e748d989c8fa7
aljubuncic <ahmedlj07@gmail.com>
2023-11-09 15:11:34 +0100

    modified FoodAndDrink class

---
+++ Models/FoodAndDrink.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GoTravnikApi.Models
+{
+    public class FoodAndDrink : TouristContent
+    {
+        
+        
+        public string Website { get; set; }
+        public string TelephoneNumber { get; set; }
+        public FoodAndDrink()
+        {
+        }
+
+        public FoodAndDrink(int id, string name, string description, string type, int idLocation, Location location, string image, string website, string telephoneNumber)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            IdLocation = idLocation;
+            Location = location;
+            Image = image;
+            Website = website;
+            TelephoneNumber = telephoneNumber;
+        }
+    }
+}
