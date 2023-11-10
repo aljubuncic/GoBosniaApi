6da8c181e203cbd2f449da3b0f2fe39103287927
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-10 11:16:31 +0100

    migrated to db

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,5 +1,7 @@
 ﻿using GoTravnikApi.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using System.Reflection.Emit;
 
 namespace GoTravnikApi.Data
 {
@@ -7,6 +9,29 @@ namespace GoTravnikApi.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<Attraction> Attractions { get; set; }
+        public DbSet<Accommodation> Accomodation { get; set; }
+        public DbSet<Activity> Activity { get; set; }
+        public DbSet<Attraction> Attraction { get; set; }
+        public DbSet<Event> Event { get; set; }
+        public DbSet<FoodAndDrink> FoodAndDrink { get; set; }
+        public DbSet<Location> Location { get; set; }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<Rating> Rating { get; set; }
+        public DbSet<Subcategory> Subcategory { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Accommodation>().ToTable(nameof(Accommodation));
+            builder.Entity<Activity>().ToTable(nameof(Activity));
+            builder.Entity<Attraction>().ToTable(nameof(Attraction));
+            builder.Entity<Event>().ToTable(nameof(Event));
+            builder.Entity<FoodAndDrink>().ToTable(nameof(FoodAndDrink));
+            builder.Entity<Location>().ToTable(nameof(Location));
+            builder.Entity<Post>().ToTable(nameof(Post));
+            builder.Entity<Rating>().ToTable(nameof(Rating));
+            builder.Entity<Subcategory>().ToTable(nameof(Subcategory));
+            base.OnModelCreating(builder);
+        }
+
     }
 }
---
+++ Migrations/20231110095700_FirstMigration.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoTravnikApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    XCoordinate = table.Column<double>(type: "float", nullable: false),
+                    YCoordinate = table.Column<double>(type: "float", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TouristContent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdLocation = table.Column<int>(type: "int", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TouristContent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TouristContent_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Activity_TouristContent_Id",
+                        column: x => x.Id,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attraction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attraction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attraction_TouristContent_Id",
+                        column: x => x.Id,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    startDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    endDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Event_TouristContent_Id",
+                        column: x => x.Id,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodAndDrink",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Website = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelephoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodAndDrink", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FoodAndDrink_TouristContent_Id",
+                        column: x => x.Id,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    PostDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Post_TouristContent_Id",
+                        column: x => x.Id,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rating",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    TextComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdTouristContent = table.Column<int>(type: "int", nullable: false),
+                    TouristContentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rating", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rating_TouristContent_TouristContentId",
+                        column: x => x.TouristContentId,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subcategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdTouristContent = table.Column<int>(type: "int", nullable: false),
+                    TouristContentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subcategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subcategory_TouristContent_TouristContentId",
+                        column: x => x.TouristContentId,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accommodation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IdRating = table.Column<int>(type: "int", nullable: false),
+                    RatingId = table.Column<int>(type: "int", nullable: false),
+                    Website = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelephoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accommodation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accommodation_Rating_RatingId",
+                        column: x => x.RatingId,
+                        principalTable: "Rating",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Accommodation_TouristContent_Id",
+                        column: x => x.Id,
+                        principalTable: "TouristContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accommodation_RatingId",
+                table: "Accommodation",
+                column: "RatingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rating_TouristContentId",
+                table: "Rating",
+                column: "TouristContentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcategory_TouristContentId",
+                table: "Subcategory",
+                column: "TouristContentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TouristContent_LocationId",
+                table: "TouristContent",
+                column: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accommodation");
+
+            migrationBuilder.DropTable(
+                name: "Activity");
+
+            migrationBuilder.DropTable(
+                name: "Attraction");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "FoodAndDrink");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "Subcategory");
+
+            migrationBuilder.DropTable(
+                name: "Rating");
+
+            migrationBuilder.DropTable(
+                name: "TouristContent");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+        }
+    }
+}
