b03b18631c8af1395697ca7e1dffd21b6e38fdf1
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 12:13:01 +0100

    added attraction repository

---
+++ Repository/AttractionRepository.cs
@@ -0,0 +1,30 @@
+﻿using GoTravnikApi.Data;
+using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTravnikApi.Repository
+{
+    public class AttractionRepository : IAttractionRepository
+    {
+        private readonly DataContext _dataContext;
+        public AttractionRepository(DataContext dataContext)
+        { 
+            _dataContext = dataContext;
+        }
+        public async Task<bool> AttractionExists(int id)
+        {
+            return await _dataContext.Attraction.AnyAsync(a => a.Id == id); 
+        }
+
+        public async Task<Attraction> GetAttraction(int id)
+        {
+            return await _dataContext.Attraction.Where(a => a.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Attraction>> GetAttractions()
+        {
+            return await _dataContext.Attraction.ToListAsync();
+        }
+    }
+}
