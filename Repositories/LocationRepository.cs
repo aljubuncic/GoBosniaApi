71c65798f4809241ff176a3dbd846f0f8791963f
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 17:27:45 +0100

    added location repository

---
+++ Repositories/LocationRepository.cs
@@ -0,0 +1,28 @@
+﻿using GoTravnikApi.Data;
+using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTravnikApi.Repositories
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public LocationRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<bool> AddLocation(Location location)
+        {
+            await _dataContext.AddAsync(location);
+            return await Save();
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _dataContext.SaveChangesAsync();
+            return saved > 0;
+        }
+    }
+}
