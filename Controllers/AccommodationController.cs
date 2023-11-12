748e2494168118a637590a98262a8b41c51243bb
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 10:37:41 +0100

    added api methods for accommodations

---
+++ Controllers/AccommodationController.cs
@@ -0,0 +1,45 @@
+﻿using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoTravnikApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccommodationController : Controller
+    {
+        private readonly IAccommodationRepository _accommodationRepository;
+        public AccommodationController(IAccommodationRepository accommodationRepository)
+        {
+            _accommodationRepository = accommodationRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<Accommodation>))]
+        public async Task<ActionResult<List<Accommodation>>> GetAccommodations()
+        {
+            var events = await _accommodationRepository.GetAccomodations();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(events);
+        }
+
+        [HttpGet("{accommodationId}")]
+        [ProducesResponseType(200, Type = typeof(Event))]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<Event>> GetEvent(int accommodationId)
+        {
+            if (!await _accommodationRepository.AccomodationExists(accommodationId))
+                return NotFound(ModelState);
+            var events = await _accommodationRepository.GetAccommodation(accommodationId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(events);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IAccommodationRepository, AccommodationRepository>();    
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
 {
