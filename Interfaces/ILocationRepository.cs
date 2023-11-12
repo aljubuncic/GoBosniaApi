ab376be678d00133244fbfabcfbd21b9de497818
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 17:27:35 +0100

    added interface for location repository

---
+++ Interfaces/ILocationRepository.cs
@@ -0,0 +1,11 @@
+﻿using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Interfaces
+{
+    public interface ILocationRepository
+    {
+        public Task<bool> AddLocation(Location location);
+
+        public Task<bool> Save();
+    }
+}
