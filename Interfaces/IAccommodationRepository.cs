0bb797339ccb7c1960d841554fff2aeba65c5020
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 10:37:17 +0100

    added interface for accommodation repository

---
+++ Interfaces/IAccommodationRepository.cs
@@ -0,0 +1,11 @@
+﻿using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Interfaces
+{
+    public interface IAccommodationRepository
+    {
+        public Task<List<Accommodation>> GetAccomodations();
+        public Task<Accommodation> GetAccommodation(int id);
+        public Task<bool> AccomodationExists(int id);
+    }
+}
