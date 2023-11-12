b5768ae35d4b1320682be6ef854ddcd00822003c
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 12:32:45 +0100

    added food and drink repository

---
+++ Repositories/FoodAndDrinkRepository.cs
@@ -0,0 +1,30 @@
+﻿using GoTravnikApi.Data;
+using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTravnikApi.Repositories
+{
+    public class FoodAndDrinkRepository : IFoodAndDrinkRepository
+    {
+        private readonly DataContext _dataContext;
+        public FoodAndDrinkRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<bool> FoodAndDrinkExists(int id)
+        {
+            return await _dataContext.FoodAndDrink.AnyAsync(fad => fad.Id == id);
+        }
+
+        public async Task<FoodAndDrink> GetFoodAndDrink(int id)
+        {
+            return await _dataContext.FoodAndDrink.Where(fad => fad.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<FoodAndDrink>> GetFoodAndDrinks()
+        {
+            return await _dataContext.FoodAndDrink.ToListAsync();
+        }
+    }
+}
