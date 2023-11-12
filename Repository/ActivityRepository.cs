e343c6d60fbf392ff3201cdc5e79082f9ffaffe5
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 11:42:52 +0100

    added activity repository

---
+++ Repository/ActivityRepository.cs
@@ -0,0 +1,30 @@
+﻿using GoTravnikApi.Data;
+using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTravnikApi.Repository
+{
+    public class ActivityRepository : IActivityRepository
+    {
+        private readonly DataContext _dataContext;
+        public ActivityRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<bool> ActivityExists(int id)
+        {
+            return await _dataContext.Accomodation.AnyAsync(a => a.Id == id);
+        }
+
+        public async Task<Activity> GetActivity(int id)
+        {
+            return await _dataContext.Activity.Where(a => a.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Activity>> GetActivities()
+        {
+            return await _dataContext.Activity.ToListAsync();
+        }
+    }
+}
