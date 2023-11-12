2cabc0dfa4c7036a4c89e31fa4d0b994d0d5fb9c
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 12:12:41 +0100

    added interface for attraction repository

---
+++ Interfaces/IAttractionRepository.cs
@@ -0,0 +1,11 @@
+﻿using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Interfaces
+{
+    public interface IAttractionRepository
+    {
+        public Task<List<Attraction>> GetAttractions();
+        public Task<Attraction> GetAttraction(int id);
+        public Task<bool> AttractionExists(int id);
+    }
+}
