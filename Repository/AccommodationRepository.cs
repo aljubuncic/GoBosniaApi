ebeb9e3c71e003c8b5fee0a6850e2c6e89bc1085
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-12 10:37:30 +0100

    added accommodation repository

---
+++ Repository/AccommodationRepository.cs
@@ -0,0 +1,30 @@
+﻿using GoTravnikApi.Data;
+using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTravnikApi.Repository
+{
+    public class AccommodationRepository : IAccommodationRepository
+    {
+        private readonly DataContext _dataContext;
+        public AccommodationRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<bool> AccomodationExists(int id)
+        {
+            return await _dataContext.Accomodation.AnyAsync(a => a.Id == id);
+        }
+
+        public async Task<Accommodation> GetAccommodation(int id)
+        {
+            return await _dataContext.Accomodation.Where(a => a.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Accommodation>> GetAccomodations()
+        {
+            return await _dataContext.Accomodation.ToListAsync();
+        }
+    }
+}
