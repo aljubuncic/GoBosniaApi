b06bc658f167786ff7a51adaecfb4b39f5d9bd42
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-13 21:37:14 +0100

    added subcategory repo

---
+++ Repositories/SubcategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using GoTravnikApi.Data;
+using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTravnikApi.Repositories
+{
+    public class SubcategoryRepository : ISubcategoryRepository
+    {
+        private readonly DataContext _dataContext;
+        public SubcategoryRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext; 
+        }
+        public async Task<bool> SubcategoriesExist(List<Subcategory> subcategories)
+        {
+            foreach(var subcategory in subcategories)
+                if(! await _dataContext.Subcategory.AnyAsync(s => s.Id == subcategory.Id))
+                    return false;
+            return true;
+        }
+    }
+}
