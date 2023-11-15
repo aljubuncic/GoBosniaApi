36331e5434b7ae72647ed17f9044578f903bfaa0
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-11-15 15:45:33 +0100

    added rating repo

---
+++ Repositories/RatingRepository.cs
@@ -0,0 +1,27 @@
+﻿using GoTravnikApi.Data;
+using GoTravnikApi.Interfaces;
+using GoTravnikApi.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace GoTravnikApi.Repositories
+{
+    public class RatingRepository : IRatingRepository
+    {
+        private readonly DataContext _dataContext;
+        public RatingRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<bool> AddRating(Rating rating)
+        {
+            _dataContext.Rating.Add(rating);
+            return await Save();
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _dataContext.SaveChangesAsync();
+            return saved > 0;
+        }
+    }
+}
