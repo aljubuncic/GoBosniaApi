1f2a231673f05d3e7011098717293fd1f9592420
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-12-03 01:34:45 +0100

    deleted id from subcategory dto

--- Dto/SubcategoryDto.cs
+++ Dto/SubcategoryDto.cs
@@ -3,18 +3,12 @@
 namespace GoTravnikApi.Dto
 {
     public class SubcategoryDto
-    {
-        public int Id { get; set; }
+    { 
         public string Name { get; set; }
 
         public SubcategoryDto()
         {
             
-        }
-        public SubcategoryDto(Subcategory subcategory)
-        {
-            Id = subcategory.Id;
-            Name = subcategory.Name;
         }
     }
 }
