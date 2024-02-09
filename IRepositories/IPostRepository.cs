7654bb0ca5e8f4faa584dbbc074362c4a8cd9f6a
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-09 02:21:39 +0100

    add interface for post repository

---
+++ IRepositories/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IRepositories
+{
+    public interface IPostRepository : ITouristContentRepository<Post>
+    {
+        public Task<List<Post>> GetBySubcategory(string subcategoryName);
+    }
+}
