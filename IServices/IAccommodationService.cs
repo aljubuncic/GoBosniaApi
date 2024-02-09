848df36651a6ce9173f64d82c6b1aad8ba7474bc
Ahmed Ljubunčić <116281740+aljubuncic@users.noreply.github.com>
2024-02-09 02:16:26 +0100

    Delete IServices directory

--- IServices/IAccommodationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using GoTravnikApi.Dto;
-using GoTravnikApi.Models;
-
-namespace GoTravnikApi.IServices
-{
-    public interface IAccommodationService : ITouristContentService<Accommodation, AccommodationDtoRequest, AccommodationDtoResponse>
-    {
-    }
-}
--- IServices/IActivityService.cs
+++
@@ -1,9 +0,0 @@
-﻿using GoTravnikApi.Dto;
-using GoTravnikApi.Models;
-
-namespace GoTravnikApi.IServices
-{
-    public interface IActivityService : ITouristContentService<Activity, ActivityDtoRequest, ActivityDtoResponse>
-    {
-    }
-}
