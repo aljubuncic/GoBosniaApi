986db8b307425996344b34456b0e0e0172427f2d
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-09 02:14:20 +0100

    add interface for accommodation repository

---
+++ IServices/IAccommodationService.cs
@@ -0,0 +1,9 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface IAccommodationService : ITouristContentService<Accommodation, AccommodationDtoRequest, AccommodationDtoResponse>
+    {
+    }
+}
