29eb07a8f000f22a487ead2ae1c4acf44f12abdb
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-10 00:51:34 +0100

    add validation for tourist content dto properties

--- Dto/TouristContentDtoRequest.cs
+++ Dto/TouristContentDtoRequest.cs
@@ -1,13 +1,17 @@
 ﻿using GoTravnikApi.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoTravnikApi.Dto
 {
     public abstract class TouristContentDtoRequest
-    { 
+    {
+        [Required(ErrorMessage = "Name must be provided")]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Location must be provided")]
         public LocationDtoRequest Location { get; set; }
         public string Image { get; set; }
+        [MinLength(1,ErrorMessage = "There must be atleast one subcategory provided")]
         public List<SubcategoryDto> Subcategories { get; set; }
         public TouristContentDtoRequest()
         {
