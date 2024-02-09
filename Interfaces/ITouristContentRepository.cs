3c55f2d5cb52432588ef8f8db00aea60b29501bb
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-10 00:45:38 +0100

    rename folder to IRepositories

--- Interfaces/ITouristContentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using GoTravnikApi.Models;
-
-namespace GoTravnikApi.Interfaces
-{
-    public interface ITouristContentRepository
-    {
-        public Task<TouristContent> GetTouristContent(int ratingId);
-    }
-}
