9a29199469c7941994c38418354221a9356fdb61
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-09 02:13:26 +0100

    add interface for repository

---
+++ IRepositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace GoTravnikApi.IRepositories
+{
+    public interface IRepository<Entity> where Entity : class
+    {
+        public Task<List<Entity>> GetAll();
+        public Task<Entity?> GetById(int id);
+        public Task<bool> Add(Entity entity);
+        public Task<bool> Update(Entity entity);
+        public Task<bool> Delete(Entity entity);
+        public Task<bool> Save();
+    }
+}
