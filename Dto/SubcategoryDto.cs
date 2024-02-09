6b843b9349790cebf66281472ca2110e3d506474
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-10 00:51:03 +0100

    add validation for subcategory dto property

--- Dto/SubcategoryDto.cs
+++ Dto/SubcategoryDto.cs
@@ -1,9 +1,11 @@
 ﻿using GoTravnikApi.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoTravnikApi.Dto
 {
     public class SubcategoryDto
-    { 
+    {
+        [Required(ErrorMessage = "Name of subcategory must be provided")]
         public string Name { get; set; }
 
         public SubcategoryDto()
