dbe9864c8a2078b9352d7f0a6686225d5d4dd0de
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-09 02:20:55 +0100

    add interface for event repository

---
+++ IRepositories/IEventRepository.cs
@@ -0,0 +1,9 @@
+﻿using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IRepositories
+{
+    public interface IEventRepository : ITouristContentRepository<Event>
+    {
+        public Task<List<Event>> FilterByDate(DateTime startDate, DateTime endDate);
+    }
+}
