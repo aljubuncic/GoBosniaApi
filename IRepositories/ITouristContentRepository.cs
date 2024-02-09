6d9e0db97b6f809045757b9745866d24157f2266
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-09 02:19:53 +0100

    add interface for tourist content repository

---
+++ IRepositories/ITouristContentRepository.cs
@@ -0,0 +1,11 @@
+﻿using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IRepositories
+{
+    public interface ITouristContentRepository<Entity> : IRepository<Entity> where Entity : TouristContent
+    {
+        public Task<List<Entity>> GetByName(string name);
+        public Task<List<Entity>> FilterBySubcategories(List<string> subcategoryNames);
+        public Task<List<Entity>> Sort(string sortOption);
+    }
+}
