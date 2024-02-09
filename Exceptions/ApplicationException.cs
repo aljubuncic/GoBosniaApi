1948f4d90ce5e62f2c0efc84907862dca872ef6f
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-10 00:53:00 +0100

    add base class for app exceptions

---
+++ Exceptions/ApplicationException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace GoTravnikApi.Exceptions
+{
+    public class ApplicationException : Exception
+    {
+        public HttpStatusCode HttpStatusCode { get; }
+        public ApplicationException(HttpStatusCode httpStatusCode, string message) : base(message) 
+        {
+            HttpStatusCode = httpStatusCode;    
+        }
+    }
+}
