6a502bbf66c9c8098b863dafb038ef3c37d7987f
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-10 00:53:27 +0100

    add custom exception for bad http request

---
+++ Exceptions/BadRequestException.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace GoTravnikApi.Exceptions
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(string message) : base(HttpStatusCode.BadRequest, message)
+        {
+        }
+    }
+}
