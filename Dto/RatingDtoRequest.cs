92ff35bf0b623cab7849aadf04aa03408ff1c0bb
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-10 00:50:39 +0100

    add validation for rating dto properties

--- Dto/RatingDtoRequest.cs
+++ Dto/RatingDtoRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace GoTravnikApi.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoTravnikApi.Dto
 {
     public class RatingDtoRequest
-    { 
+    {
+        [Required]
+        [Range(0,5)]
         public int Value { get; set; }
         public string TextComment { get; set; }
 
