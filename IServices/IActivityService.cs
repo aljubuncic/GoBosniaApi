e45809d0da4859b98a6807c655eb511aafd16704
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-09 02:14:36 +0100

    add interface for activity repository

---
+++ IServices/IActivityService.cs
@@ -0,0 +1,9 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface IActivityService : ITouristContentService<Activity, ActivityDtoRequest, ActivityDtoResponse>
+    {
+    }
+}
