6e74f026f4cf3062657f6fcd99ec442af8e86a1a
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-09 02:22:07 +0100

    add interface for subcategory repository

---
+++ IRepositories/ISubcategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IRepositories
+{
+    public interface ISubcategoryRepository : IRepository<Subcategory>
+    {
+        public Task<bool> SubcategoriesExist(List<string> subcategoryNames);
+        public Task<Subcategory?> GetSubcategory(string name);
+    }
+}
