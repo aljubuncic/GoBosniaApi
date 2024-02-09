39a3d8ac4079801eb52a22eee34a8d0adace4fa5
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-10 00:50:05 +0100

    add validation for location dto properties

--- Dto/LocationDtoRequest.cs
+++ Dto/LocationDtoRequest.cs
@@ -1,9 +1,14 @@
-﻿namespace GoTravnikApi.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoTravnikApi.Dto
 {
     public class LocationDtoRequest
     {
+        [Required(ErrorMessage = "X coordinate must be provided")]
         public double XCoordinate { get; set; }
+        [Required(ErrorMessage = "Y coordinate must be provided")]
         public double YCoordinate { get; set; }
+        [Required(ErrorMessage = "Address must be provided")]
         public string Address { get; set; }
 
         public LocationDtoRequest()
