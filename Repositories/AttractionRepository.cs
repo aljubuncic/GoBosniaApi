2ea1be2745cef1d242b11b77432eca350ddea0c1
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-11 12:28:39 +0100

    modify attraction repo

--- Repositories/AttractionRepository.cs
+++ Repositories/AttractionRepository.cs
@@ -1,69 +1,16 @@
 ﻿using GoTravnikApi.Data;
 using GoTravnikApi.Interfaces;
+using GoTravnikApi.IRepositories;
 using GoTravnikApi.Models;
+using GoTravnikApi.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoTravnikApi.Repository
 {
-    public class AttractionRepository : IAttractionRepository
+    public class AttractionRepository : TouristContentRepository<Attraction>, IAttractionRepository
     {
-        private readonly DataContext _dataContext;
-        public AttractionRepository(DataContext dataContext)
-        { 
-            _dataContext = dataContext;
-        }
-        public async Task<bool> AttractionExists(int id)
+        public AttractionRepository(DataContext dataContext) : base(dataContext)
         {
-            return await _dataContext.Attraction.AnyAsync(a => a.Id == id); 
-        }
-
-        public async Task<Attraction> GetAttraction(int id)
-        {
-            return await _dataContext.Attraction
-                .Where(a => a.Id == id)
-                .Include(a => a.Location)
-                .Include(a => a.Ratings)
-                .Include(a => a.Subcategories)
-                .FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Attraction>> GetAttractions()
-        {
-            return await _dataContext.Attraction
-                .Include(a => a.Location)
-                .Include(a => a.Ratings)
-                .ToListAsync();
-        }
-
-        public async Task<List<Attraction>> GetAttractions(string searchName)
-        {
-            return await _dataContext.Attraction
-                .Where(a => a.Name.ToLower().Contains(searchName.ToLower()))
-                .Include(a => a.Location)
-                .Include(a => a.Ratings)
-                .ToListAsync();
-        }
-        public async Task<bool> AddAttraction(Attraction attraction)
-        {
-            await _dataContext.Location.AddAsync(attraction.Location);
-
-            await _dataContext.Attraction.AddAsync(attraction);
-
-            return await Save();
-        }
-        public async Task<bool> Save()
-        {
-            var saved = await _dataContext.SaveChangesAsync();
-            return saved > 0;
-        }
-
-        public async Task<List<Attraction>> FilterAttractions(List<string> subcategoryNames)
-        {
-            var query = _dataContext.Attraction.AsQueryable();
-            foreach (var subcategory in subcategoryNames)
-                query = query.Where(e => e.Subcategories.Any(sub => sub.Name == subcategory) == true);
-
-            return await query.ToListAsync();
         }
     }
 }
