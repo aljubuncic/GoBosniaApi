c4d62be9b032517a48453c6cfbac419fb211aaf4
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-11 12:29:14 +0100

    modify post repo

--- Repositories/PostRepository.cs
+++ Repositories/PostRepository.cs
@@ -1,64 +1,22 @@
 ﻿using GoTravnikApi.Data;
 using GoTravnikApi.Interfaces;
+using GoTravnikApi.IRepositories;
 using GoTravnikApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoTravnikApi.Repositories
 {
-    public class PostRepository : IPostRepository
+    public class PostRepository : TouristContentRepository<Post>, IPostRepository
     {
-        private readonly DataContext _dataContext;
-
-
-        public PostRepository(DataContext dataContext)
+        private readonly DataContext dataContext;
+        public PostRepository(DataContext dataContext) : base(dataContext)
         {
-            _dataContext = dataContext;
-        }
-        
-
-        public async Task<bool> AddPost(Post post)
-        {
-            await _dataContext.Location.AddAsync(post.Location);
-
-            await _dataContext.Post.AddAsync(post);
-
-            return await Save();
         }
 
-        public async Task<bool> PostExists(int id)
+        public async Task<List<Post>> GetBySubcategory(string subcategoryName)
         {
-            return await _dataContext.Post.AnyAsync(fad => fad.Id == id);
-        }
-
-        public async Task<Post> GetPost(int id)
-        {
-            return await _dataContext.Post
-                .Where(fad => fad.Id == id)
-                .Include(fad => fad.Location)
-                .Include(fad => fad.Subcategories)
-                .FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Post>> GetPosts()
-        {
-            return await _dataContext.Post
-                .Include(fad => fad.Location)
-                .Include(fad => fad.Subcategories)
-                .ToListAsync();
-        }
-
-        public async Task<bool> Save()
-        {
-            var saved = await _dataContext.SaveChangesAsync();
-            return saved > 0;
-        }
-
-        public Task<List<Post>> GetPosts(string subcategoryName)
-        {
-            return _dataContext.Post
-                .Where(x =>  x.Subcategories.Any(s => s.Name == subcategoryName) == true)
-                .Include(fad => fad.Location)
-                .ToListAsync();
+            var posts = await dataContext.Post.Where(x => x.Subcategories.Any(s => s.Name == subcategoryName)).ToListAsync();
+            return posts;
         }
     }
 
