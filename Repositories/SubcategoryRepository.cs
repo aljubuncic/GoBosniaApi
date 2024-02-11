c9ba964a3fb19f2d6e0598a8912e4ff20415399f
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-11 12:35:43 +0100

    modify subcategory repo

--- Repositories/SubcategoryRepository.cs
+++ Repositories/SubcategoryRepository.cs
@@ -1,41 +1,33 @@
 ﻿using GoTravnikApi.Data;
 using GoTravnikApi.Interfaces;
+using GoTravnikApi.IRepositories;
 using GoTravnikApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoTravnikApi.Repositories
 {
-    public class SubcategoryRepository : ISubcategoryRepository
+    public class SubcategoryRepository : Repository<Subcategory>,ISubcategoryRepository
     {
         private readonly DataContext _dataContext;
-        public SubcategoryRepository(DataContext dataContext)
+
+        public SubcategoryRepository(DataContext dataContext) : base(dataContext)
         {
-            _dataContext = dataContext; 
+            _dataContext = dataContext;
         }
+
+        public async Task<Subcategory?> GetSubcategory(string name)
+        {
+            var subcategory = await _dataContext.Subcategory.Where(x => x.Name == name).SingleOrDefaultAsync();
+            return subcategory;
+        }
+
         public async Task<bool> SubcategoriesExist(List<string> subcategoryNames)
         {
             foreach(var subcategoryName in subcategoryNames)
                 if(! await _dataContext.Subcategory.AnyAsync(s => s.Name.Equals(subcategoryName)))
                     return false;
             return true;
-        }
-
-       public async Task<bool> AddSubcategory(Subcategory subcategory)
-       {
-            await _dataContext.AddAsync(subcategory);
-            return await Save();
-       }
-
-        public async Task<bool> Save()
-        {
-            var saved = await _dataContext.SaveChangesAsync();
-            return saved > 0;
-        }
-
-        public async Task<Subcategory> GetSubcategory(string name)
-        {
-            return await _dataContext.Subcategory.Where(x => x.Name.Equals(name)).FirstOrDefaultAsync();
         }
     }
 }
