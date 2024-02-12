27480a3e8f258c959d8bdf95349e9bd773888b74
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-12 17:57:11 +0100

    add interface for service for usual methods on dtos

---
+++ IServices/IService.cs
@@ -0,0 +1,17 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface IService<Entity, EntityRequestDto, EntityResponseDto>
+        where Entity : class
+        where EntityRequestDto : class
+        where EntityResponseDto : class
+    {
+        public Task<List<EntityResponseDto>> GetAll();
+        public Task<EntityResponseDto> GetById(int id);
+        public Task<int> Add(EntityRequestDto entityRequestDto);
+        public Task Delete(int id);
+
+    }
+}
