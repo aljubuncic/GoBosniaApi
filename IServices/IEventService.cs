8d3a2a409b2415b9dfcc00c2eaa9e5c678084340
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-12 17:58:57 +0100

    add interface for event service

---
+++ IServices/IEventService.cs
@@ -0,0 +1,10 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface IEventService : ITouristContentService<Event, EventDtoRequest, EventDtoResponse>
+    {
+        public Task<List<EventDtoResponse>> GetByDateRange(DateTime startDate, DateTime endDate);
+    }
+}
