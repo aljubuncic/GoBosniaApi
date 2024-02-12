51e5ba739e6ba7778ea6c35318bcb65848c09c58
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-12 17:59:32 +0100

    add interface for post service

---
+++ IServices/IPostService.cs
@@ -0,0 +1,10 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface IPostService : ITouristContentService<Post, PostDtoRequest, PostDtoResponse>   
+    {
+        public Task<List<PostDtoResponse>> GetBySubcategory(string subcategoryName);
+    }
+}
