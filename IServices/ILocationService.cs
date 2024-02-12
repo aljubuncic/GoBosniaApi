82b053a1c35df95c6815a033f6a07d67d2df1c40
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-12 17:59:24 +0100

    add interface for location service

---
+++ IServices/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface ILocationService : IService<Location, LocationDtoRequest,  LocationDtoResponse>
+    {
+    }
+}
