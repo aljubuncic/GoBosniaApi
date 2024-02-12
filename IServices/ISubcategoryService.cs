df1455438df070caa7c3747528852db137845f2c
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-12 17:59:50 +0100

    add interface for subcategory service

---
+++ IServices/ISubcategoryService.cs
@@ -0,0 +1,12 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.IRepositories;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface ISubcategoryService : IService<Subcategory, SubcategoryDto, SubcategoryDto>
+    {
+        public Task<SubcategoryDto> GetSubcategory(string name);
+        public Task<bool> SubcategoriesExist(List<string> subcategoryNames);
+    }
+}
