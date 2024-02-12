df8dc7f03f9b176e1979722d53dfe33bf67521c5
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-12 17:59:39 +0100

    add interface for rating service

---
+++ IServices/IRatingService.cs
@@ -0,0 +1,10 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface IRatingService : IService<Rating, RatingDtoRequest, RatingDtoResponse>
+    {
+        public Task ApproveRating(int id);
+    }
+}
