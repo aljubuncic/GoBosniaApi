6a9f9e829009d1a954267acdc6e5dac8d3d77e19
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-13 17:44:22 +0100

    add accommodation service

---
+++ Services/AccommodationService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GoTravnikApi.Dto;
+using GoTravnikApi.IRepositories;
+using GoTravnikApi.IServices;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Services
+{
+    public class AccommodationService
+        : TouristContentService<Accommodation, AccommodationDtoRequest, AccommodationDtoResponse>, IAccommodationService
+    {
+        public AccommodationService(IAccommodationRepository accommodationRepository, ISubcategoryRepository subcategoryRepository, IMapper mapper) : base(accommodationRepository, subcategoryRepository, mapper)
+        {
+        }
+    }
+}
