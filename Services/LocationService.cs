c437e8f1b28fa9b05234423b8237ece0120c6353
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-13 17:45:06 +0100

    add location service

---
+++ Services/LocationService.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GoTravnikApi.Dto;
+using GoTravnikApi.IRepositories;
+using GoTravnikApi.IServices;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Services
+{
+    public class LocationService : Service<Location, LocationDtoRequest, LocationDtoResponse>, ILocationService
+    {
+        public LocationService(ILocationRepository locationRepository, IMapper mapper) : base(locationRepository, mapper)
+        {
+        }
+    }
+}
