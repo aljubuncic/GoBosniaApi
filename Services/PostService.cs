f4ce19d14b64ce23699cd20413f1b722be55b4e9
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-13 17:45:14 +0100

    add post service

---
+++ Services/PostService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using GoTravnikApi.Dto;
+using GoTravnikApi.IRepositories;
+using GoTravnikApi.IServices;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Services
+{
+    public class PostService
+        : TouristContentService<Post, PostDtoRequest, PostDtoResponse>, IPostService
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly ISubcategoryRepository _subcategoryRepository;
+        private readonly IMapper _mapper;
+        public PostService(IPostRepository postRepository, ISubcategoryRepository subcategoryRepository, IMapper mapper) : base(postRepository, subcategoryRepository,mapper)
+        {
+            _postRepository = postRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<PostDtoResponse>> GetBySubcategory(string subcategoryName)
+        {
+            var postDtoResponses = _mapper.Map<List<PostDtoResponse>>(await _postRepository.GetBySubcategory(subcategoryName));
+            return postDtoResponses;
+        }
+    }
+}
