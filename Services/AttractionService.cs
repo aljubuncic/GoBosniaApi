83493f6253682f7573719d65c4ffe2ce92377d9c
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-13 17:44:39 +0100

    add attraction service

---
+++ Services/AttractionService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GoTravnikApi.Dto;
+using GoTravnikApi.IRepositories;
+using GoTravnikApi.IServices;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Services
+{
+    public class AttractionService
+        : TouristContentService<Attraction, AttractionDtoRequest, AttractionDtoResponse>, IAttractionService
+    {
+        public AttractionService(IAttractionRepository attractionRepository, ISubcategoryRepository subcategoryRepository, IMapper mapper) : base(attractionRepository, subcategoryRepository, mapper)
+        {
+        }
+    }
+}
