195499a5eb3f87365a6991c3057f8410e52254b4
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-13 17:44:57 +0100

    add food and drink service

---
+++ Services/FoodAndDrinkService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GoTravnikApi.Dto;
+using GoTravnikApi.IRepositories;
+using GoTravnikApi.IServices;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.Services
+{
+    public class FoodAndDrinkService
+        : TouristContentService<FoodAndDrink, FoodAndDrinkDtoRequest, FoodAndDrinkDtoResponse>, IFoodAndDrinkService
+    {
+        public FoodAndDrinkService(IFoodAndDrinkRepository foodAndDrinkRepository, ISubcategoryRepository subcategoryRepository, IMapper mapper) : base(foodAndDrinkRepository, subcategoryRepository, mapper)
+        {
+        }
+    }
+}
