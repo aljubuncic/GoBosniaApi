c0f785749cb269e00e113be70c950cd14a7d992d
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-13 17:43:53 +0100

    add service for usual methods on entities

---
+++ Services/Service.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using GoTravnikApi.Exceptions;
+using GoTravnikApi.IRepositories;
+using GoTravnikApi.IServices;
+using GoTravnikApi.Models;
+using GoTravnikApi.Repositories;
+
+namespace GoTravnikApi.Services
+{
+    public abstract class Service<Entity, EntityRequestDto, EntityResponseDto>
+        : IService<Entity, EntityRequestDto, EntityResponseDto>
+        where Entity : class
+        where EntityRequestDto : class
+        where EntityResponseDto : class
+    {
+        private readonly IRepository<Entity> _repository;
+        private readonly IMapper _mapper;
+        public Service(IRepository<Entity> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<int> Add(EntityRequestDto touristContentDtoRequest)
+        {
+            var touristContent = _mapper.Map<Entity>(touristContentDtoRequest);
+            try 
+            {
+                var addedEntityId = await _repository.Add(touristContent);
+                return addedEntityId;
+            }
+            catch(InternalServerErrorException)
+            {
+                throw;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            var touristContent = await _repository.GetById(id) ?? throw new NotFoundException($"Entity with {id} does not exist in the database");
+            try
+            {
+                await _repository.Delete(touristContent);
+            }
+            catch (InternalServerErrorException)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<EntityResponseDto>> GetAll()
+        {
+            var touristContentDtoResponses = _mapper.Map<List<EntityResponseDto>>(await _repository.GetAll());
+            return touristContentDtoResponses;
+        }
+
+        public async Task<EntityResponseDto> GetById(int id)
+        {
+            var touristContent = await _repository.GetById(id) ?? throw new NotFoundException($"Entity with {id} does not exist in the database");
+            var touristContentDtoResponse = _mapper.Map<EntityResponseDto>(touristContent);
+            return touristContentDtoResponse;
+        }
+    }
+}
