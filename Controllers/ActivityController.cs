bf5f00bc018fa9e6ec70311e4b3f8a86eaff822a
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-15 01:10:17 +0100

    add controller for activty

--- Controllers/ActivityController.cs
+++ Controllers/ActivityController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using GoTravnikApi.Dto;
-using GoTravnikApi.Interfaces;
+
+using GoTravnikApi.IServices;
 using GoTravnikApi.Models;
 using GoTravnikApi.Repositories;
-using GoTravnikApi.Repository;
+
+using GoTravnikApi.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,148 +13,38 @@ namespace GoTravnikApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ActivityController : Controller
+    public class ActivityController : 
+        TouristContentController<Activity, ActivityDtoRequest, ActivityDtoResponse>
     {
-        private readonly IActivityRepository _activityRepository;
-        private readonly ISubcategoryRepository _subcategoryRepository;
-        private readonly IRatingRepository _ratingRepository;
-        private readonly IMapper _mapper;
-        public ActivityController(IActivityRepository activityRepository, ISubcategoryRepository subcategoryRepository, IRatingRepository ratingRepository, IMapper mapper)
+        private readonly IActivityService _activityService;
+        private readonly ISubcategoryService _subcategoryService;
+        private readonly IRatingService _ratingService;
+
+        public ActivityController(IActivityService activityService, ISubcategoryService subcategoryService, IRatingService ratingService) : base(activityService, subcategoryService, ratingService)
         {
-            _activityRepository = activityRepository;
-            _subcategoryRepository = subcategoryRepository;
-            _ratingRepository = ratingRepository;   
-            _mapper = mapper;
+            _activityService = activityService;
+            _subcategoryService = subcategoryService;
+            _ratingService = ratingService;
         }
 
-        [HttpGet]
-        [ProducesResponseType(200, Type = typeof(List<ActivityDtoResponse>))]
-        public async Task<ActionResult<List<ActivityDtoResponse>>> GetActivities()
+        [HttpGet("sort/{sortOption}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<List<ActivityDtoResponse>>> GetSortedAccommodations(string sortOption)
         {
-            var activityDtos = await _activityRepository.GetActivities();
+            var activityResponseDtos = await _activityService.Sort(sortOption);
 
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            return Ok(activityDtos);
-        }
-
-        [HttpGet("{id:int}")]
-        [ProducesResponseType(200, Type = typeof(ActivityDtoResponse))]
-        [ProducesResponseType(404)]
-        public async Task<ActionResult<ActivityDtoResponse>> GetActivity(int id)
-        {
-            if (!await _activityRepository.ActivityExists(id))
-                return NotFound(ModelState);
-            var activityDto = _mapper.Map<ActivityDtoResponse> (await _activityRepository.GetActivity(id));
-
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            return Ok(activityDto);
-        }
-
-        [HttpGet("{name}")]
-        [ProducesResponseType(200, Type = typeof(List<ActivityDtoResponse>))]
-        public async Task<ActionResult<List<ActivityDtoResponse>>> GetActivities(string name)
-        {
-            var activitiyDtos = _mapper.Map<List<ActivityDtoResponse>>(await _activityRepository.GetActivities(name));
-
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            return Ok(activitiyDtos);
+            return Ok(activityResponseDtos);
         }
 
         [HttpGet("filter")]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
-        public async Task<ActionResult<List<ActivityDtoResponse>>> GetFilteredAndOrderedActivities([FromQuery] List<string> subcategory,[FromQuery] string? sortOption)
+        public async Task<ActionResult<List<ActivityDtoResponse>>> GetFilteredAccommodations([FromQuery] List<string> subcategoryNames)
         {
-            var activityDtos = _mapper.Map<List<ActivityDtoResponse>>(await _activityRepository.FilterAndOrderActivities(subcategory, sortOption));
+            var activityResponseDtos = await _activityService.GetBySubcategories(subcategoryNames);
 
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            return Ok(activityDtos);
-        }
-
-        [HttpPost]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        public async Task<ActionResult> AddActivity([FromBody] ActivityDtoRequest activityDtoRequest)
-        {
-            if (activityDtoRequest == null)
-                return BadRequest(ModelState);
-
-            if (activityDtoRequest.Location == null)
-                return BadRequest(ModelState);
-
-            if (!await _subcategoryRepository.SubcategoriesExist(activityDtoRequest.Subcategories.Select(x => x.Name).ToList()))
-            {
-                ModelState.AddModelError("error", "Subcategory does not exist in the database");
-                return StatusCode(400, ModelState);
-            }
-
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            List<Subcategory> subcategories = new List<Subcategory>();
-
-            foreach (var subcategoryDto in activityDtoRequest.Subcategories)
-            {
-                var subcategory = await _subcategoryRepository.GetSubcategory(subcategoryDto.Name);
-                subcategories.Add(subcategory);
-            }
-            Activity activity = _mapper.Map<Activity>(activityDtoRequest);
-            activity.Subcategories = subcategories;
-
-            if (!await _activityRepository.AddActivity(activity))
-            {
-                ModelState.AddModelError("error", "Database saving error");
-                return StatusCode(500, ModelState);
-            }
-
-            return Ok ("Succesfully added");
-
-        }
-
-        [HttpPost("rating/{activityId:int}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        public async Task<ActionResult> AddRating(int activityId, [FromBody] RatingDtoRequest ratingDtoRequest)
-        {
-            if (ratingDtoRequest == null)
-                return BadRequest(ModelState);
-
-            if (!await _activityRepository.ActivityExists(activityId))
-            {
-                ModelState.AddModelError("error", "Activity does not exist in the database");
-                return StatusCode(400, ModelState);
-            }
-
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var activity = await _activityRepository.GetActivity(activityId);
-            var rating = _mapper.Map<Rating>(ratingDtoRequest);
-
-            activity.Ratings.Add(rating);
-
-            if (!await _ratingRepository.AddRating(rating))
-            {
-                ModelState.AddModelError("error", "Database updating error");
-                return StatusCode(500, ModelState);
-            }
-
-            if (!await _activityRepository.UpdateActivity(activity))
-            {
-                ModelState.AddModelError("error", "Database updating error");
-                return StatusCode(500, ModelState);
-            }
-
-            return Ok("Successfully added");
-
+            return Ok(activityResponseDtos);
         }
 
     }
