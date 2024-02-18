774aa4c3bc7914fdbd62267252538d646aa16eb9
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-19 00:10:04 +0100

    add interface for rated tourist content service

---
+++ IServices/IRatedTouristContentService.cs
@@ -0,0 +1,14 @@
+﻿using GoTravnikApi.Dto;
+using GoTravnikApi.Models;
+
+namespace GoTravnikApi.IServices
+{
+    public interface IRatedTouristContentService<Entity, EntityRequestDto, EntityResponseDto>
+        : ITouristContentService<Entity, EntityRequestDto, EntityResponseDto>
+        where Entity : TouristContent
+        where EntityRequestDto : TouristContentDtoRequest
+        where EntityResponseDto : TouristContentDtoResponse
+    {
+        public Task<int> AddRating(int id, RatingDtoRequest ratingDtoRequest);
+    }
+}
