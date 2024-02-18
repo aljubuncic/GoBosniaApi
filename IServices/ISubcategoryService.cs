7d2f92b92f60b58e622a00b70c5c6358dbec028d
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-19 00:10:16 +0100

    refactor with new dtos

--- IServices/ISubcategoryService.cs
+++ IServices/ISubcategoryService.cs
@@ -4,9 +4,9 @@ using GoTravnikApi.Models;
 
 namespace GoTravnikApi.IServices
 {
-    public interface ISubcategoryService : IService<Subcategory, SubcategoryDto, SubcategoryDto>
+    public interface ISubcategoryService : IService<Subcategory, SubcategoryDtoRequest, SubcategoryDtoResponse>
     {
-        public Task<SubcategoryDto> GetSubcategory(string name);
+        public Task<SubcategoryDtoResponse> GetSubcategory(string name);
         public Task<bool> SubcategoriesExist(List<string> subcategoryNames);
     }
 }
