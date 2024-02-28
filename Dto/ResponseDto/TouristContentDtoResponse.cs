6c6891df74be6356134cd86bca4b000704b9be37
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-02-28 22:14:51 +0100

    modify tourist content dto

--- Dto/ResponseDto/TouristContentDtoResponse.cs
+++ Dto/ResponseDto/TouristContentDtoResponse.cs
@@ -8,7 +8,6 @@ namespace GoTravnikApi.Dto.ResponseDto
         public string Name { get; set; }
         public string Description { get; set; }
         public LocationDtoResponse Location { get; set; }
-        public string Image { get; set; }
         public List<SubcategoryDtoResponse> Subcategories { get; set; }
         public List<ImageDtoResponse> Images { get; set; }
         public TouristContentDtoResponse()
